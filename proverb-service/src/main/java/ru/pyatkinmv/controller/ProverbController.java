04bdd1cd312380112aff832cbac36f5b67657ebb
Maxim Pyatkin <pyatkinmv@gmail.com>
2020-05-04 17:08:48 +0300

    Package proverb supplying into proverb-service module; create root pom

--- proverb-service/src/main/java/ru/pyatkinmv/controller/ProverbController.java
+++ proverb-service/src/main/java/ru/pyatkinmv/controller/ProverbController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.pyatkinmv.model.Proverb;
 import ru.pyatkinmv.service.ProverbService;
 
 @Controller
@@ -16,7 +15,7 @@ public class ProverbController {
     private final ProverbService proverbService;
 
     @GetMapping("/supply")
-    public ResponseEntity<Proverb> supply() {
-        return new ResponseEntity<>(proverbService.supply(), HttpStatus.OK);
+    public ResponseEntity<String> supply() {
+        return new ResponseEntity<>(proverbService.supply().toString(), HttpStatus.OK);
     }
 }
